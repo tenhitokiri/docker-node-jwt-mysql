647e2f812e72bbdfd599c640da4f0b54db2f6a50
Christian Mejia <tenhitokiri@gmail.com>
2020-04-29 13:17:16 -0400

    pruebas consumiendo otra api

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const path = require('path');
 const morgan = require('morgan');
 const session = require('express-session');
 const mySqlStore = require('express-mysql-session');
+const fetch = require('node-fetch');
 
 const passport = require('passport');
 const verify = require('./modules/auth/verify');
@@ -11,15 +12,13 @@ const verify = require('./modules/auth/verify');
 //activar modo desarrollo
 const modo = process.env.PORT;
 if (!modo) {
-    const dotenv = require('dotenv').config();
-    console.log('modo dev')
+	const dotenv = require('dotenv').config();
+	console.log('modo dev');
 }
 
-const {
-    database
-} = require('./keys');
+const { database } = require('./keys');
 
-const app = express()
+const app = express();
 
 //Configuración de la aplicación
 
@@ -27,21 +26,21 @@ const app = express()
 app.use(express.json());
 // Express Session
 app.use(
-    session({
-        secret: process.env.SECRET_KEY,
-        resave: false,
-        saveUninitialized: false,
-        store: new mySqlStore(database)
-    })
+	session({
+		secret            : process.env.SECRET_KEY,
+		resave            : false,
+		saveUninitialized : false,
+		store             : new mySqlStore(database)
+	})
 );
 
 //RUTAS
 app.get('/', (req, res) => {
-    const message = `La api esta en /API. la clave secreta es ${secret_key}`
-    res.json({
-        message
-    })
-})
+	const message = `La api esta en /API. la clave secreta es ${process.env.SECRET_KEY}`;
+	res.json({
+		message
+	});
+});
 
 //Ruta de autenticación
 const rutaUsuarios = require('./modules/usuarios/usuarios');
@@ -51,13 +50,20 @@ app.use('/api/users', rutaUsuarios);
 const rutaAuth = require('./modules/auth/auth');
 app.use('/api/auth', rutaAuth);
 
+app.use('/server', async (req, res) => {
+	//const roles = await fetch('http://10.10.0.2/api/roles');
+	const roles = await fetch('https://jsonplaceholder.typicode.com/posts');
+	console.log(roles);
+	//res.json(roles);
+	res.send(roles);
+});
 
 //404 handle
 app.use((req, res, next) => {
-    res.status(404).json({
-        err: "Error, Ruta no encontrada"
-    })
-    next();
+	res.status(404).json({
+		err : 'Error, Ruta no encontrada'
+	});
+	next();
 });
 
 module.exports = app;
