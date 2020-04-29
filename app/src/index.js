7d77596d97520198aba39474acceb57af076fdb6
Christian Mejia <tenhitokiri@gmail.com>
2020-04-29 13:23:04 -0400

    consumir otra api

--- app/src/index.js
+++ app/src/index.js
@@ -12,11 +12,13 @@ const verify = require('./modules/auth/verify');
 //activar modo desarrollo
 const modo = process.env.PORT;
 if (!modo) {
-	const dotenv = require('dotenv').config();
-	console.log('modo dev');
+    const dotenv = require('dotenv').config();
+    console.log('modo dev');
 }
 
-const { database } = require('./keys');
+const {
+    database
+} = require('./keys');
 
 const app = express();
 
@@ -26,15 +28,15 @@ const app = express();
 app.use(express.json());
 // Express Session
 app.use(
-	session({
-		secret            : process.env.SECRET_KEY,
-		resave            : false,
-		saveUninitialized : false,
-		store             : new mySqlStore(database)
-	})
+    session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: new mySqlStore(database)
+    })
 );
 
-app.use(async (req, resp, next) => {
+/* app.use(async (req, resp, next) => {
     //console.log('Time:', Date.now());
     //resp.set('X-XSS-Protection', "0");
    //console.log("headers : ", JSON.stringify(req.headers));
@@ -44,15 +46,15 @@ app.use(async (req, resp, next) => {
     //req.header('Access-Control-Expose-Headers');
     //req.body = {json:"auth"}; 
   next();
-})
+}) */
 
 
 //RUTAS
 app.get('/', (req, res) => {
-	const message = `La api esta en /API. la clave secreta es ${process.env.SECRET_KEY}`;
-	res.json({
-		message
-	});
+    const message = `La api esta en /API. la clave secreta es ${process.env.SECRET_KEY}`;
+    res.json({
+        message
+    });
 });
 
 //Ruta de autenticación
@@ -64,19 +66,19 @@ const rutaAuth = require('./modules/auth/auth');
 app.use('/api/auth', rutaAuth);
 
 app.use('/server', async (req, res) => {
-	//const roles = await fetch('http://10.10.0.2/api/roles');
-	const roles = await fetch('https://jsonplaceholder.typicode.com/posts');
-	console.log(roles);
-	//res.json(roles);
-	res.send(roles);
+    //const roles = await fetch('http://10.10.0.2/api/roles');
+    const roles = await fetch('https://jsonplaceholder.typicode.com/posts');
+    console.log(roles);
+    //res.json(roles);
+    res.send(roles);
 });
 
 //404 handle
 app.use((req, res, next) => {
-	res.status(404).json({
-		err : 'Error, Ruta no encontrada'
-	});
-	next();
+    res.status(404).json({
+        err: 'Error, Ruta no encontrada'
+    });
+    next();
 });
 
 module.exports = app;
